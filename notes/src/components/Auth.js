93e4e1909ebf178a24928d372f5e82f053f808fa
TheGameFreak720 <luis.alvarez@commithub.com>
2019-02-14 22:06:11 -0500

    created auth component

---
+++ notes/src/components/Auth.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class Auth extends Component {
+    render() {
+        <div>
+            <p>Authorization</p>
+        </div>
+    }
+}
+
+export default Auth;
